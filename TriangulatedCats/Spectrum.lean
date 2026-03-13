b6c966bfd4618839293f2d292481f28a6278fddb
Leopold Mayer <leomayer@uw.edu>
2026-03-13 12:22:06 -0700

    finished proof that Thick(C) is the terminal support datum

--- TriangulatedCats/Spectrum.lean
+++ TriangulatedCats/Spectrum.lean
@@ -1,4 +1,5 @@
 import Mathlib.CategoryTheory.Triangulated.Subcategory
+import Mathlib.CategoryTheory.ConcreteCategory.Basic
 import Mathlib.Topology.Sets.Closeds
 
 /-
@@ -82,10 +83,11 @@ variable {C : Type*} [Category C] [Preadditive C] [HasZeroObject C] [HasShift C 
   [∀ n : ℤ, Functor.Additive (shiftFunctor C n)] [Pretriangulated C]
 
 def Z (I : Set C) : Set (ThickSubcategory C) := {P : (ThickSubcategory C) | I ∩ P = ⊥}
+def D (I : Set C) : Set (ThickSubcategory C) := {P : (ThickSubcategory C) | ∃ X : C, X ∈ (I ∩ P)}
 
 @[simp]
-def mem_Z_iff {I : Set C} {P : ThickSubcategory C} : P ∈ Z I ↔ (I ∩ P) = ⊥ := by simp [Z]
-def not_mem_Z_iff {I : Set C} {P : ThickSubcategory C} : P ∉ Z I ↔ ∃ X, X ∈ I ∩ P := by
+theorem mem_Z_iff {I : Set C} {P : ThickSubcategory C} : P ∈ Z I ↔ (I ∩ P) = ⊥ := by simp [Z]
+theorem not_mem_Z_iff {I : Set C} {P : ThickSubcategory C} : P ∉ Z I ↔ ∃ X, X ∈ I ∩ P := by
   contrapose
   convert mem_Z_iff
   simp [Set.eq_empty_iff_forall_notMem]
@@ -93,57 +95,28 @@ def not_mem_Z_iff {I : Set C} {P : ThickSubcategory C} : P ∉ Z I ↔ ∃ X, X 
 theorem ThickSubcategory.ne_empty (P : ThickSubcategory C) : P.carrier ≠ ∅ :=
   (Set.nonempty_iff_ne_empty).mp ⟨0, P.zero_mem⟩
 
-instance : TopologicalSpace (ThickSubcategory C) := TopologicalSpace.ofClosed {Z I | I : Set C}
-    ⟨⊤, Set.eq_empty_iff_forall_notMem.mpr (fun P => by simp [Z]; apply P.ne_empty)⟩
-    (by
-      intro A hA
-      use ⋃₀ {I | Z I ∈ A}
-      ext P
-      constructor
-      . intro hP
-        rw [Set.mem_sInter]
-        rw [mem_Z_iff] at hP
-        rintro X hX
-        obtain ⟨I, hI⟩ := hA hX
-        rw [←hI, mem_Z_iff, ←le_bot_iff]
-        rw [←hI] at hX
-        rw [←le_bot_iff] at hP
-        apply le_trans _ hP
-        apply Set.inter_subset_inter_left _ (Set.subset_sUnion_of_mem _)
-        exact hX
-      . intro hP
-        rw [mem_Z_iff]
-        rw [Set.mem_sInter] at hP
-        ext X
-        constructor
-        . rintro ⟨hx₁, hx₂⟩
-          obtain ⟨I, hZI : Z I ∈ A, hXI⟩ := hx₁
-          specialize hP (Z I) hZI
-          rw [mem_Z_iff] at hP
-          rw [←hP]
-          exact ⟨hXI, hx₂⟩
-        . intro hX; cases hX
-    )
-    (by
-      rintro _ ⟨I, rfl⟩ _ ⟨J, rfl⟩
-      use {C | ∃ A ∈ I, ∃ B ∈ J, C = (A ⊞ B)}
-      ext P
-      constructor <;> contrapose
-      . rw [←Set.mem_compl_iff, Set.compl_union]
-        rintro ⟨hPI, hPJ⟩
-        rw [Set.mem_compl_iff, not_mem_Z_iff] at hPI hPJ
-        rw [not_mem_Z_iff]
-        obtain ⟨A, hAI, hAP⟩ := hPI
-        obtain ⟨B, hBJ, hBP⟩ := hPJ
-        exact ⟨A ⊞ B, ⟨A, hAI, B, hBJ, rfl⟩, P.biprod_mem hAP hBP⟩
-      . intro hP
-        rw [not_mem_Z_iff] at hP
-        rcases hP with ⟨_ ,⟨A, hAI, B, hBI, rfl⟩, hP⟩
-        rw [←Set.mem_compl_iff, Set.compl_union]
-        constructor <;> rw [Set.mem_compl_iff, not_mem_Z_iff]
-        refine ⟨A, hAI, P.smd_mem_left hP⟩
-        refine ⟨B, hBI, P.smd_mem_right hP⟩
-    )
+
+instance : TopologicalSpace (ThickSubcategory C) where
+  IsOpen := {D I | I : Set C}
+  isOpen_univ := ⟨⊤, Set.eq_univ_of_forall (fun P => ⟨0, Set.mem_univ _, P.zero_mem⟩)⟩
+  isOpen_inter := by
+    rintro _ _ ⟨I, rfl⟩ ⟨J, rfl⟩
+    use {C | ∃ A ∈ I, ∃ B ∈ J, C = (A ⊞ B)}
+    ext P
+    constructor
+    . exact fun ⟨_, ⟨⟨A, hA, B, hB, rfl⟩, hXP⟩⟩ => ⟨⟨A, hA, P.smd_mem_left hXP⟩, ⟨B, hB, P.smd_mem_right hXP⟩⟩
+    . exact fun ⟨⟨A, hAI, hAP⟩, ⟨B, hBI, hBP⟩⟩ => ⟨A ⊞ B, ⟨A, hAI, B, hBI, rfl⟩, P.biprod_mem hAP hBP⟩
+  isOpen_sUnion := by
+    intro A hA
+    use ⋃₀ {I | D I ∈ A}
+    ext P
+    constructor
+    . exact fun ⟨X, ⟨I, hI, hIP⟩, hXP⟩ => ⟨D I, hI, ⟨X, hIP, hXP⟩⟩
+    . rintro ⟨I, hI, hIP⟩
+      obtain ⟨J, hJ⟩ := hA I hI
+      rw [←hJ] at hIP hI
+      obtain ⟨X, hX, hXP⟩ := hIP
+      exact ⟨X, ⟨J, hI, hX⟩, hXP⟩
 
 end topology
 
@@ -166,32 +139,25 @@ supp_obj₃' {T : Triangle C} (hT : T ∈ distTriang C) : supp (T.obj₃) ≤ su
 instance SupportDatum.TopologicalSpace (X : SupportDatum C) : TopologicalSpace X.X := X.hX
 
 @[simps]
-def UnivSupport : SupportDatum C where
+abbrev UnivSupport : SupportDatum C where
 X := ThickSubcategory C
 hX := inferInstance
-supp a := ⟨Z {a}, by use {a}; simp⟩
-supp_zero' := by ext; simp
+supp a := ⟨{P | a ∉ P}, by use {a}; ext; simp [D]⟩
+supp_zero' := by simp; rfl
 supp_biprod' a b := by
   ext P
-  simp
+  show (a ⊞ b) ∉ P ↔ a ∉ P ∨ b ∉ P
   contrapose!
   exact ⟨fun h => ⟨P.smd_mem_left h, P.smd_mem_right h⟩, fun h => P.biprod_mem h.left h.right⟩
 supp_shift' a := by
   ext P
-  simp
+  show (shiftFunctor C 1).obj a ∉ P ↔ a ∉ P
   contrapose!
   refine ⟨P.mem_of_shift_mem (i := 1), P.shift_mem (i := 1)⟩
-supp_obj₃' {T} hT := by
-  intro P hP₃
-  simp at *
+supp_obj₃' {T} hT P hP₃ := by
   by_cases hP₁ : T.obj₁ ∈ P
-  . right
-    simp
-    intro hP₂
-    apply hP₃
-    apply P.obj₃_mem hT hP₁ hP₂
-  . left
-    simpa
+  . exact Or.inr (fun hP₂ => hP₃ (P.obj₃_mem hT hP₁ hP₂))
+  . exact Or.inl hP₁
 
 end supportdatum
 
@@ -199,21 +165,19 @@ namespace SupportDatum
 
 variable {C : Type*} [Category C] [Preadditive C] [HasZeroObject C] [HasShift C ℤ]
   [∀ n : ℤ, Functor.Additive (shiftFunctor C n)] [Pretriangulated C]
-variable (X : SupportDatum C)
-
-example (a b : C) : a ⟶ b := 0
+variable (X Y : SupportDatum C)
 
 @[simp]
-def supp_zero : X.supp 0 = ⊥ := X.supp_zero'
-def supp_biprod (a b : C) : X.supp (a ⊞ b) = X.supp (a) ⊔ X.supp (b) := X.supp_biprod' a b
-def supp_obj₁ {T : Triangle C} (hT : T ∈ distTriang C) : X.supp (T.obj₁) ≤ X.supp (T.obj₂) ⊔ X.supp (T.obj₃) := by
+theorem supp_zero : X.supp 0 = ⊥ := X.supp_zero'
+theorem supp_biprod (a b : C) : X.supp (a ⊞ b) = X.supp (a) ⊔ X.supp (b) := X.supp_biprod' a b
+theorem supp_obj₁ {T : Triangle C} (hT : T ∈ distTriang C) : X.supp (T.obj₁) ≤ X.supp (T.obj₂) ⊔ X.supp (T.obj₃) := by
   rw [←X.supp_shift' T.obj₁]
   exact X.supp_obj₃' (T := T.rotate) (rot_of_distTriang T hT)
-def supp_obj₂ {T : Triangle C} (hT : T ∈ distTriang C) : X.supp (T.obj₂) ≤ X.supp (T.obj₁) ⊔ X.supp (T.obj₃) := by
+theorem supp_obj₂ {T : Triangle C} (hT : T ∈ distTriang C) : X.supp (T.obj₂) ≤ X.supp (T.obj₁) ⊔ X.supp (T.obj₃) := by
   rw [←X.supp_shift' T.obj₁, sup_comm]
   exact X.supp_obj₁ (T := T.rotate) (rot_of_distTriang T hT)
-def supp_obj₃ {T : Triangle C} (hT : T ∈ distTriang C) : X.supp (T.obj₃) ≤ X.supp (T.obj₁) ⊔ X.supp (T.obj₂) := X.supp_obj₃' hT
-def supp_iso {a b : C} (h : Nonempty (a ≅ b)) : X.supp a = X.supp b := by
+theorem supp_obj₃ {T : Triangle C} (hT : T ∈ distTriang C) : X.supp (T.obj₃) ≤ X.supp (T.obj₁) ⊔ X.supp (T.obj₂) := X.supp_obj₃' hT
+theorem supp_iso {a b : C} (h : Nonempty (a ≅ b)) : X.supp a = X.supp b := by
   obtain ⟨φ⟩ := h
   let T : Triangle C := Triangle.mk (Z := 0) φ.hom 0 0
   have hT : T ∈ distTriang C := by
@@ -225,7 +189,7 @@ def supp_iso {a b : C} (h : Nonempty (a ≅ b)) : X.supp a = X.supp b := by
   . convert X.supp_obj₂ hT
     simp [T]
 @[simp]
-def supp_shift {i : ℤ} (a : C) : X.supp (a⟦i⟧) = X.supp (a) := by
+theorem supp_shift {i : ℤ} (a : C) : X.supp (a⟦i⟧) = X.supp (a) := by
   induction i with
   | zero => exact X.supp_iso ⟨(shiftFunctorZero C ℤ).app a⟩
   | succ i ih =>
@@ -236,10 +200,15 @@ def supp_shift {i : ℤ} (a : C) : X.supp (a⟦i⟧) = X.supp (a) := by
     symm
     exact Eq.trans (X.supp_iso ⟨(shiftFunctorAdd' C _ _ _ (sub_add_cancel _ _)).app a⟩) (X.supp_shift' _)
 
+@[ext]
 structure SupportDatumHom (X Y : SupportDatum C) where
 f : X.X → Y.X
 cont : Continuous f
 supp : ∀ a : C, X.supp a = f⁻¹' Y.supp a
+
+instance : FunLike (SupportDatumHom X Y) X.X Y.X where
+  coe f := f.f
+  coe_injective' a b (h : a.f = b.f) := by ext; rw [h]
 
 instance : Category (SupportDatum C) where
 Hom X Y := SupportDatumHom X Y
@@ -247,39 +216,49 @@ id X := { f := id, cont := continuous_id, supp a := rfl}
 comp {X Y Z} f g :=
   { f := g.f ∘ f.f, cont := Continuous.comp g.cont f.cont, supp a := by ext; simp [f.supp, g.supp]}
 
+instance : ConcreteCategory (SupportDatum C) SupportDatumHom where
+  hom := id
+  ofHom := id
+
 def isTerminal_UnivSupportDatum : IsTerminal (UnivSupport C) := IsTerminal.ofUniqueHom
   (fun X =>
     { f x :=
       { carrier := {a | x ∉ (X.supp a)},
-        zero_mem' := by
-          simp
-          apply Set.notMem_empty,
-        shift_mem' {i} {a} (ha : x ∉ _) := by
-          rw [←X.supp_shift a] at ha
-          exact ha
-        iso_mem' {a} {b} (ha : x ∉ _) (h) := by
-          rw [X.supp_iso h] at ha
-          exact ha,
-        obj₃_mem' {T} hT (h₁ : x ∉ X.supp T.obj₁) (h₂ : x ∉ X.supp T.obj₂) := by
-          intro h₃
-          cases X.supp_obj₃ hT h₃
-          . apply h₁
-            assumption
-          . apply h₂
-            assumption
+        zero_mem' : x ∉ X.supp 0 := by
+          rw [X.supp_zero]
+          apply Set.notMem_empty
+        shift_mem' {i} {a} ha := by
+          show x ∉ X.supp (a⟦i⟧)
+          rwa [X.supp_shift a]
+        iso_mem' {a} {b} (ha : x ∉ _) h := by
+          show x ∉ X.supp b
+          rwa [←X.supp_iso h]
+        obj₃_mem' {T} hT (h₁ : x ∉ X.supp T.obj₁) (h₂ : x ∉ X.supp T.obj₂) h₃ :=
+          Or.elim (X.supp_obj₃ hT h₃) h₁ h₂
         smd_mem' {a} {b} (hab : _ ∉ _) := by
-          intro ha
-          apply hab
-          rw [X.supp_biprod]
-          left
-          exact ha
+          show x ∉ X.supp a
+          rw [X.supp_biprod] at hab
+          exact fun ha => hab (Or.inl ha)
       }
-      cont := by
-        rw [continuous_iff_isClosed]
-        intro s hs
-        sorry
-      supp a := sorry
+      cont := ⟨by
+        rintro _ ⟨I, rfl⟩
+        apply isClosed_compl_iff.mp
+        have H : IsClosed (⋂₀ {(X.supp a).carrier | a ∈ I}) := by
+          apply isClosed_sInter
+          rintro _ ⟨a, ha, rfl⟩
+          exact (X.supp a).isClosed
+        convert H
+        ext x
+        simp [D]
+        show (∀ a ∈ I, ¬(x ∉ X.supp a)) ↔ ∀ a ∈ I, x ∈ X.supp a
+        aesop⟩
+      supp a := Set.ext (fun x => (by tauto : x ∈ X.supp a ↔ ¬ x ∉ X.supp a))
     })
-  sorry
+  (fun X m => by
+    ext x a
+    show a ∈ m.f x ↔ x ∉ (X.supp a : Set X.X)
+    rw [m.supp]
+    simp
+  )
 
 end SupportDatum
