2ef2003609f19293278b670dbe4e4da389b6d4ea
Leopold Mayer <leomayer@uw.edu>
2026-03-25 18:19:23 -0700

    finished proof that biprod triangle is distinguished

--- TriangulatedCats/BiprodTriangle.lean
+++ TriangulatedCats/BiprodTriangle.lean
@@ -18,22 +18,49 @@ noncomputable
 def BiprodTriangle (T T' : Triangle C) : Triangle C := Triangle.mk (biprod.map T.mor₁ T'.mor₁)
     (biprod.map T.mor₂ T'.mor₂) (biprod.map T.mor₃ T'.mor₃ ≫ (Functor.mapBiprod _ _ _).inv)
 
+/- A very strange proof: mathlib already has that the product of distinguished triangles
+  is distinguished, so I bootstrap that by showing the biprodTriangle is isomorphic
+  to a product indexed by WalkingPair. There are some definitional quagmires, but
+  this ultimately avoids a longer diagram chase. Ideally there would be
+    1) a predicate P on triangles expressing that Hom is (co)homological
+    2) a proof that P is closed under sums, products, etc
+    3) a proof that the five lemma holds for triangles satisfying P
+  -/
 def dist_biprodTriangle (hT : T ∈ distTriang C) (hT' : T' ∈ distTriang C) :
     BiprodTriangle T T' ∈ distTriang C := by
-  obtain ⟨C, g, h, H⟩ := distinguished_cocone_triangle (biprod.map T.mor₁ T'.mor₁)
-  obtain ⟨φ, _, _⟩ :=
-    complete_distinguished_triangle_morphism _ _ hT H biprod.inl biprod.inl (by aesop)
-  obtain ⟨φ', _, _⟩ :=
-    complete_distinguished_triangle_morphism _ _ hT' H biprod.inr biprod.inr (by aesop)
+  have H := productTriangle_distinguished (fun j => WalkingPair.casesOn j T T')
+    (fun j => WalkingPair.casesOn j hT hT')
   apply isomorphic_distinguished _ H
-  let Φ : BiprodTriangle T T' ⟶ Triangle.mk (biprod.map T.mor₁ T'.mor₁) g h :=
-    Triangle.homMk _ _ (𝟙 _) (𝟙 _) (biprod.desc φ φ')
-  have : IsIso Φ := Triangle.isIso_of_isIsos Φ (IsIso.id _) (IsIso.id _)
-    (by
-      refine isIso_of_yoneda_map_bijective _ (fun A => ⟨?_, ?_⟩)
-      . intro a₁ a₂ ha
-        sorry
-      . intro a
-        sorry
-    )
-  apply asIso Φ
+  apply Triangle.isoMk _ _ ?_ ?_ ?_ ?_ ?_ ?_
+  . apply Iso.mk ?_ ?_ (?_) (?_)
+    . apply Pi.lift fun j => WalkingPair.casesOn j ?_ ?_
+      exact biprod.fst
+      exact biprod.snd
+    . apply biprod.lift (Pi.π _ WalkingPair.left) (Pi.π _ WalkingPair.right)
+    . aesop
+    . simp only [productTriangle_obj₁, BiprodTriangle_obj₁]
+      ext j
+      cases j <;> simp
+  . apply Iso.mk ?_ ?_ (?_) (?_)
+    . apply Pi.lift fun j => WalkingPair.casesOn j ?_ ?_
+      exact biprod.fst
+      exact biprod.snd
+    . apply biprod.lift (Pi.π _ WalkingPair.left) (Pi.π _ WalkingPair.right)
+    . aesop
+    . simp only [productTriangle_obj₂, BiprodTriangle_obj₂]
+      ext j
+      cases j <;> simp
+  . apply Iso.mk ?_ ?_ (?_) (?_)
+    . apply Pi.lift fun j => WalkingPair.casesOn j ?_ ?_
+      exact biprod.fst
+      exact biprod.snd
+    . apply biprod.lift (Pi.π _ WalkingPair.left) (Pi.π _ WalkingPair.right)
+    . aesop
+    . simp only [productTriangle_obj₃, BiprodTriangle_obj₃]
+      ext j
+      cases j <;> simp
+  all_goals
+  simp
+  ext j <;>
+  cases j <;>
+  simp [←Functor.map_comp]
