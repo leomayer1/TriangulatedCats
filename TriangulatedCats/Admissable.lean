34c68ea455076d868f53881d58a79dafd3066a4e
Leopold Mayer <leomayer@uw.edu>
2026-04-03 14:55:15 -0700

    Defined the left and right semiorthogonal complements as thick subcategories

--- TriangulatedCats/Admissable.lean
+++ TriangulatedCats/Admissable.lean
@@ -1,8 +1,12 @@
 import TriangulatedCats.ThickSubcategory
+import Mathlib.Algebra.Category.Grp.Zero
+import Mathlib.CategoryTheory.ObjectProperty.Shift
 
 noncomputable section
 
-open CategoryTheory Limits ObjectProperty Functor Pretriangulated
+open CategoryTheory Limits ObjectProperty Functor Preadditive Pretriangulated ZeroObject
+
+open IsStableUnderShift
 
 section sod
 
@@ -26,6 +30,7 @@ variable {C : Type*} [Category C] [HasZeroObject C] [Preadditive C]
 
 variable (A B : ThickSubcategory C) (S : SOD C)
 
+
 /- TODO:
   · left admissible category
   · right admissible category
@@ -44,8 +49,43 @@ def IsRightAdmissible := IsRightAdjoint (ι A.carrier)
 theorem IsLeftAdmissible_SOD : IsLeftAdmissible S.A := sorry
 theorem IsRightAdmissible_SOD : IsRightAdmissible S.B := sorry
 
-def RightComplement (B : Set C) : ThickSubcategory C := sorry
-def LeftComplement (A : Set C) : ThickSubcategory C := sorry
+def RightComplement (B : Set C) [IsStableUnderShift B ℤ] : ThickSubcategory C where
+  carrier := {c : C | ∀ b ∈ B, ∀ f : b ⟶ c, f = 0}
+  zero_mem' b hb f := zero_of_to_zero f
+  shift_mem' {i} {a} ha b hb f := by
+    rw [←map_eq_zero_iff (shiftFunctor C (-i)), ←IsIso.comp_right_eq_zero _ (shiftShiftNeg _ _).hom]
+    apply ha (b⟦-i⟧) (le_shift B _ _ hb)
+  iso_mem' {a b ha} φ c hc f := by
+    rw [←IsIso.comp_right_eq_zero _ φ.inv]
+    apply ha _ hc
+  obj₃_mem' {T} hT hT₁ hT₂ b hb f := by
+    have H : f ≫ T.mor₃ = 0 := by
+      rw [←map_eq_zero_iff (shiftFunctor C (-1 : ℤ)), ←IsIso.comp_right_eq_zero _ (shiftShiftNeg _ _).hom]
+      apply hT₁ _ (le_shift B _ _ hb)
+    obtain ⟨g, rfl⟩ := Triangle.coyoneda_exact₃ T hT _ H
+    rw [(hT₂ b hb g), zero_comp]
+  smd_mem' {_ _ h} _ hb _ := zero_of_comp_mono biprod.inl (h _ hb _)
+
+def LeftComplement (A : Set C) [IsStableUnderShift A ℤ] : ThickSubcategory C where
+  carrier := {c : C | ∀ a ∈ A, ∀ f : c ⟶ a, f = 0}
+  zero_mem' _ _ f := zero_of_from_zero f
+  shift_mem' {i} {b} hb a ha f := by
+    rw [←map_eq_zero_iff (shiftFunctor C (-i)), ←IsIso.comp_left_eq_zero  (shiftShiftNeg _ _).inv _]
+    apply hb (a⟦-i⟧) (le_shift A _ _ ha)
+  iso_mem' {a b ha} φ c hc f := by
+    rw [←IsIso.comp_left_eq_zero φ.hom _]
+    apply ha _ hc
+  obj₃_mem' {T} hT hT₁ hT₂ a ha f := by
+    obtain ⟨g, rfl⟩ := Triangle.yoneda_exact₃ T hT f (hT₂ _ ha _)
+    have hg : g = 0 := by
+      rw [←map_eq_zero_iff (shiftFunctor C (-1)), ←IsIso.comp_left_eq_zero  (shiftShiftNeg _ _).inv _]
+      apply hT₁ (a⟦-1⟧) (le_shift A _ _ ha)
+    rw [hg, comp_zero]
+  smd_mem' {_ _ h} _ hb _ := zero_of_epi_comp biprod.fst (h _ hb _)
+
+notation "ᗮ" A => LeftComplement A
+notation A "ᗮ" => RightComplement A
+
 
 theorem exists_SOD_left (ha : IsLeftAdmissible A) : ∃ S : SOD C, A = S.A := sorry
 theorem exists_SOD_right (ha : IsRightAdmissible B) : ∃ S : SOD C, B = S.B := sorry
